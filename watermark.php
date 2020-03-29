fe353099874383a04cdb7e38c09495875ae1c286
Vitaliy Pitvalo <av.tehnik@gmail.com>
2020-03-29 13:14:09 +0300

    Update watermark.php

--- watermark.php
+++ watermark.php
@@ -10,117 +10,140 @@ $workDir = $_SERVER['PWD'];
 $replace = false;
 
 $output = $workDir . '/' . FOLDER_NAME;
+$file = null;
 
 if (array_key_exists('1', $argv)) {
-    if ($argv['1'] == '--help' || $argv['1'] == '-h') {
-        echo 'use -r to replace images, otherwise photos will be saved to watermark folder';
-    } elseif ($argv['1'] == '-r' || $argv['1'] == '-h') {
-        $replace = true;
-        $output  = $workDir;
+  if (is_file($workDir . '/' . $argv['1'])) {
+    $file = $workDir . '/' . $argv['1'];
+  } else if ($argv['1'] == '--help' || $argv['1'] == '-h') {
+    echo 'use -r to replace images, otherwise photos will be saved to watermark folder';
+  } elseif ($argv['1'] == '--install') {
+    if (!is_dir('/etc/watermark') && !$file) {
+      mkdir('/etc/watermark');
     }
+    copy(__DIR__ . '/Pacifico.ttf', '/etc/watermark/Pacifico.ttf');
+    copy(__DIR__ . '/watermark', '/usr/local/bin/watermark');
+    echo "finish".PHP_EOL;
+    exit;
+  } elseif ($argv['1'] == '-r' || $argv['1'] == '-h') {
+    $replace = true;
+    $output = $workDir;
+  }
+
+}
+
+if (!is_dir($output) && !$file) {
+  mkdir($output);
 }
 
 
-if ( ! is_dir($output)) {
-    mkdir($output);
-}
-
-
-function watermark($img, $folder, $name = null)
+function watermark($img, $folder, $filename)
 {
 
-    $filename = pathinfo($img, PATHINFO_BASENAME);
 
-    $image = new Imagick($img);
+  $image = new Imagick($img);
 
-    $draw = new ImagickDraw();
-    /* Black text */
-    $draw->setFillColor('#ffffff');
-    $draw->setStrokeColor('#C4C4C4');
-$draw->setStrokeWidth(2);
+  $draw = new ImagickDraw();
+  /* Black text */
+  $draw->setFillColor('#ffffff');
+  $draw->setStrokeColor('#C4C4C4');
+  $draw->setStrokeWidth(2);
 //$draw->setStrokeAntialias(true);
 //$draw->setTextAntialias(true);
 //    $draw->setFillAlpha(0.5);
 
-    /* Font properties */
-    $draw->setFont('/etc/watermark/Pacifico.ttf');
-    $text = 'Vitaliy Pitvalo';
+  /* Font properties */
+  $draw->setFont('/etc/watermark/Pacifico.ttf');
+  $text = 'Vitaliy Pitvalo';
 
-    $orientation = $image->getImageOrientation();
-
-
-    if ($orientation == 6 || $orientation == 8) {
-    } else {
-        $draw->setFontSize(70);
-    }
-    $draw->setFontSize(100);
-
-    $textData = $image->queryFontMetrics($draw, $text);
-
-    $textData['textWidth'];
-    $textData['textHeight'];
+  $orientation = $image->getImageOrientation();
 
 
-    $offset = 200;
+  if ($orientation == 6 || $orientation == 8) {
+  } else {
+    $draw->setFontSize(70);
+  }
+  $draw->setFontSize(100);
+
+  $textData = $image->queryFontMetrics($draw, $text);
+
+  $textData['textWidth'];
+  $textData['textHeight'];
+
+
+  $offset = 200;
 
 //    print_r($textData);
-    $orientation = $image->getImageOrientation();
+  $orientation = $image->getImageOrientation();
 
-    $angle = 0;
+  $angle = 0;
 
-    $draw->setGravity(\Imagick::GRAVITY_SOUTHEAST);
-    if ($orientation == 6 || $orientation == 8) {
-        if ($orientation == 8) {
-            $angle = 90;
-            $y     = ($offset+$textData['textWidth'])-180;
-            $x     = $offset-95;
-            $draw->setGravity(\Imagick::GRAVITY_SOUTHWEST);
+  $draw->setGravity(\Imagick::GRAVITY_SOUTHEAST);
+  if ($orientation == 6 || $orientation == 8) {
+    if ($orientation == 8) {
+      $angle = 90;
+      $y = ($offset + $textData['textWidth']) - 180;
+      $x = $offset - 95;
+      $draw->setGravity(\Imagick::GRAVITY_SOUTHWEST);
 
-        } else {
-            $angle = -90;
-            $y     = $offset - $textData['textHeight'] - $textData['descender'];
-            $x     = $offset;
-            $draw->setGravity(\Imagick::GRAVITY_NORTHEAST);
-        }
     } else {
-        $y = 50;
-        $x = 50;
+      $angle = -90;
+      $y = $offset - $textData['textHeight'] - $textData['descender'];
+      $x = $offset;
+      $draw->setGravity(\Imagick::GRAVITY_NORTHEAST);
     }
+  } else {
+    $y = 50;
+    $x = 50;
+  }
 //    $image->annotateImage($draw, $x-3, $y-3, $angle, $text);
 //    $draw->setFillColor('#1a8bff');
-    $image->annotateImage($draw, $x, $y, $angle, $text);
+  $image->annotateImage($draw, $x, $y, $angle, $text);
 
-    $image->writeImage($folder . '/' . $filename); //also works
+  $image->writeImage($folder . '/' . $filename); //also works
 
 }
 
-$files = scandir($workDir);
+
+if ($file) {
+
+  $filename = pathinfo($file, PATHINFO_BASENAME);
+
+  $output = pathinfo($file, PATHINFO_DIRNAME);
+
+  watermark($file, $output, "wm_".$filename);
+
+} else {
 
 
-$newFiles = [];
-foreach ($files as $file) {
+  $files = scandir($workDir);
+
+
+  $newFiles = [];
+  foreach ($files as $file) {
 
     $filepath = $workDir . '/' . $file;
 
     if (is_file($file)) {
 
-        $extension = strtolower(pathinfo($file,PATHINFO_EXTENSION));
-        if (in_array($extension,['jpg','jpeg','png'])) {
-            $newFiles[$file] = $filepath;
-        }
+      $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+      if (in_array($extension, ['jpg', 'jpeg', 'png'])) {
+        $newFiles[$file] = $filepath;
+      }
     }
-}
+  }
 
 
+  $count = count($newFiles);
+  $index = 1;
 
+  foreach ($newFiles as $name => $path) {
+    echo number_format(($index / $count) * 100, 2) . "% - $name                                                           \r";
+    $filename = pathinfo($path, PATHINFO_BASENAME);
 
-$count = count($newFiles);
-$index = 1;
-
-foreach($newFiles as $name=> $path){
-    echo number_format(($index/$count)*100,2)."% - $name                                                           \r";
-    watermark($path, $output);
+    watermark($path, $output, $filename);
     $index++;
+  }
 }
 
-echo 'Finish';
+echo PHP_EOL.'Finish'.PHP_EOL;
